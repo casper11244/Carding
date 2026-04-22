41a8dea7a1d06b3a0b21c6a7d5a5c92154ecb48d
Casper <casper@ejemplo.com>
2026-04-21 20:29:34 -0600

    segundo commit

---
+++ APICard/src/test/java/card/APICard/ApiCardApplicationTests.java
@@ -0,0 +1,13 @@
+package card.APICard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
