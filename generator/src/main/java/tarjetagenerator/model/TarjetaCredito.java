d2bdbe1cbace3b3c4424f831a09a96b3678d94b5
Casper <tuemail@ejemplo.com>
2026-04-22 23:59:15 -0600

    segunda vercion de generadores de tarejetas

--- generator/src/main/java/tarjetagenerator/model/TarjetaCredito.java
+++ generator/src/main/java/tarjetagenerator/model/TarjetaCredito.java
@@ -1,34 +1,4 @@
 package tarjetagenerator.model;
 
-import java.time.LocalDate;
-
 public class TarjetaCredito {
-    private final String numero;
-    private final int año;
-    private final int mes;
-    private final String cvv;
-    private final TipoTarjeta tipo;
-
-    public TarjetaCredito(String numero, int año, int mes, String cvv, TipoTarjeta tipo) {
-        this.numero = numero;
-        this.año = año;
-        this.mes = mes;
-        this.cvv = cvv;
-        this.tipo = tipo;
-    }
-
-    public String getNumero() { return numero; }
-    public int getAño() { return año; }
-    public int getMes() { return mes; }
-    public String getCvv() { return cvv; }
-    public TipoTarjeta getTipo() { return tipo; }
-
-    public String formatoEspecificado() {
-        return String.format("%s|%d|%02d|%s", numero, año, mes, cvv);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%-20s %s", tipo.getNombre(), formatoEspecificado());
-    }
 }
--- generator/src/main/java/tarjetagenerator/model/TipoTarjeta.java
+++ generator/src/main/java/tarjetagenerator/model/TipoTarjeta.java
@@ -1,32 +1,4 @@
 package tarjetagenerator.model;
 
-public enum TipoTarjeta {
-    VISA("4", "Visa", 16),
-    MASTERCARD("5", "MasterCard", 16),
-    AMEX("34", "American Express", 15),
-    DISCOVER("6011", "Discover", 16),
-    DINERS("300", "Diners Club", 14),
-    JCB("35", "JCB", 16);
-
-    private final String prefijo;
-    private final String nombre;
-    private final int longitud;
-
-    TipoTarjeta(String prefijo, String nombre, int longitud) {
-        this.prefijo = prefijo;
-        this.nombre = nombre;
-        this.longitud = longitud;
-    }
-
-    public String getPrefijo() { return prefijo; }
-    public String getNombre() { return nombre; }
-    public int getLongitud() { return longitud; }
-
-    public static TipoTarjeta fromString(String tipo) {
-        try {
-            return valueOf(tipo.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-    }
+public class TipoTarjeta {
 }
